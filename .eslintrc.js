06383d2236295f007710a2bbcba2f8234af126e8
Colin Gorrie <colin@colingorrie.com>
2019-01-29 08:23:15 -0500

    get eslint playing nicely with ts and jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'jest', 'import'],
+  settings: {
+    'import/parsers': {
+      'typescript-eslint-parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
+  env: {
+    'jest/globals': true,
+  },
   rules: {
     indent: 'off',
     '@typescript-eslint/indent': ['error', 2],
